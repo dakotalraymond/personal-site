d41062fe881c558f72a56f7a5fe475237e92bcf4
Dakota Raymond <dakota@askalmanac.com>
2020-02-04 20:48:52 -0700

    Add pages and crude styling

--- draft-blog/src/components/NavBar/NavBar.tsx
+++ draft-blog/src/components/NavBar/NavBar.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Styles from "./NavBar.module.scss";
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
-  return <div>Hello</div>;
+  return (
+    <div className={Styles.container}>
+      <Link to="/">Home</Link>
+      <Link to="/editor">Editor</Link>
+    </div>
+  );
 };
 
 export default NavBar;
--- draft-blog/src/screens/Editor/Editor.tsx
+++ draft-blog/src/screens/Editor/Editor.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Styles from "./Editor.module.scss";
+
 const Editor = () => {
-  return <div>Editor</div>;
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.containerInner}>Editor</div>
+    </div>
+  );
 };
 
 export default Editor;
