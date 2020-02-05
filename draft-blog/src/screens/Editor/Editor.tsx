f7a22fe4d8ec2ced877435ad98548f75f4a67613
Dakota Raymond <dakota@askalmanac.com>
2020-02-04 20:54:20 -0700

    Add draft js editor

--- draft-blog/src/screens/Editor/Editor.tsx
+++ draft-blog/src/screens/Editor/Editor.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 import Styles from "./Editor.module.scss";
+import ContentEditor from "../../components/ContentEditor/ContentEditor";
 
 const Editor = () => {
   return (
     <div className={Styles.container}>
-      <div className={Styles.containerInner}>Editor</div>
+      <div className={Styles.containerInner}>
+        <ContentEditor />
+      </div>
     </div>
   );
 };
