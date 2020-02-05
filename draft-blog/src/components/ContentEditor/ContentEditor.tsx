0566cb6e81b82662490939e1829d5e6ad41e08a2
Dakota Raymond <dakota@askalmanac.com>
2020-02-05 08:27:39 -0700

    Add local content saving

--- draft-blog/src/components/ContentEditor/ContentEditor.tsx
+++ draft-blog/src/components/ContentEditor/ContentEditor.tsx
@@ -1,13 +1,47 @@
-import React from "react";
-import { Editor, EditorState } from "draft-js";
+import React, { useEffect } from "react";
+import {
+  Editor,
+  EditorState,
+  ContentState,
+  convertToRaw,
+  convertFromRaw
+} from "draft-js";
 
 const ContentEditor = () => {
   const [editorState, setEditorState] = React.useState(
     EditorState.createEmpty()
   );
+
+  useEffect(() => {
+    const content = window.localStorage.getItem("content");
+
+    if (content) {
+      setEditorState(
+        EditorState.createWithContent(convertFromRaw(JSON.parse(content)))
+      );
+    }
+  }, []);
+
+  const saveContent = (content: ContentState) => {
+    window.localStorage.setItem(
+      "content",
+      JSON.stringify(convertToRaw(content))
+    );
+  };
+
+  const handleChange = (newState: EditorState) => {
+    const currentContent = editorState.getCurrentContent();
+    const newContent = newState.getCurrentContent();
+
+    if (currentContent !== newContent) {
+      saveContent(newContent);
+    }
+    setEditorState(newState);
+  };
+
   return (
     <div>
-      <Editor editorState={editorState} onChange={setEditorState} />
+      <Editor editorState={editorState} onChange={handleChange} />
     </div>
   );
 };
