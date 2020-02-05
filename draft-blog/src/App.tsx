6b4d1f5eb828253b434b3f7701030b45d26ab8bf
Dakota Raymond <dakota@askalmanac.com>
2020-02-04 20:24:56 -0700

    Add react router

--- draft-blog/src/App.tsx
+++ draft-blog/src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import Editor from "./screens/Editor/Editor";
+import Home from "./screens/Home/Home";
 
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/editor" component={Editor} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
